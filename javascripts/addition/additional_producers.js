da483b2b855c3fa5020ad1474d1761122419414f
STCFCYT <pspindler@gmail.com>
2026-03-17 17:09:20 -0700

    Update additional_producers.js

--- javascripts/addition/additional_producers.js
+++ javascripts/addition/additional_producers.js
@@ -1,5 +1,19 @@
 setInterval(() => {
     updateAPGain();
     AP += apgain;
+    updateAPRONEgain();
+    APRone += apronegain
+    updateAPRTWOgain();
+    APRtwo += aprtwogain
+    updateAPRTHREEgain();
+    APRthree += aprthreegain
+    updateAPRFOURgain();
+    APRfour += aprfourgain
+    updateAPRFIVEgain();
+    APRfive += aprfivegain
+    updateAPRSIXgain();
+    APRsix += aprsixgain
+    updateAPRSEVENgain();
+    APRseven += aprsevengain
     updateUI();
 }, 50);
