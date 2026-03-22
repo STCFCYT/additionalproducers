18a52f3d453d5ad33d14c550a673413a3988f4c6
STCFCYT <pspindler@gmail.com>
2026-03-22 11:30:30 -0700

    Update additional_producers.js

--- javascripts/addition/additional_producers.js
+++ javascripts/addition/additional_producers.js
@@ -8,16 +8,46 @@ setInterval(() => {
     }
     updateAPRONEgain();
     APRone += apronegain * 0.05;
+    if ((APRtwo !== 0) || (multiplications !== 0)) {
+        document.getElementById("aprthreesection").style.display = "block";
+    } else {
+        document.getElementById("aprthreesection").style.display = "none";
+    }
     updateAPRTWOgain();
     APRtwo += aprtwogain * 0.05;
+    if ((APRthree !== 0) || (multiplications !== 0)) {
+        document.getElementById("aprfoursection").style.display = "block";
+    } else {
+        document.getElementById("aprfoursection").style.display = "none";
+    }
     updateAPRTHREEgain();
     APRthree += aprthreegain * 0.05;
+    if ((APRfour !== 0) || (multiplications !== 0)) {
+        document.getElementById("aprfivesection").style.display = "block";
+    } else {
+        document.getElementById("aprfivesection").style.display = "none";
+    }
     updateAPRFOURgain();
     APRfour += aprfourgain * 0.05;
+    if ((APRfive !== 0) || (multiplications !== 0)) {
+        document.getElementById("aprsixsection").style.display = "block";
+    } else {
+        document.getElementById("aprsixsection").style.display = "none";
+    }
     updateAPRFIVEgain();
     APRfive += aprfivegain * 0.05;
+    if ((APRsix !== 0) || (multiplications !== 0)) {
+        document.getElementById("aprsevensection").style.display = "block";
+    } else {
+        document.getElementById("aprsevensection").style.display = "none";
+    }
     updateAPRSIXgain();
     APRsix += aprsixgain * 0.05;
+    if ((APRseven !== 0) || (multiplications !== 0)) {
+        document.getElementById("apreightsection").style.display = "block";
+    } else {
+        document.getElementById("apreightsection").style.display = "none";
+    }
     updateAPRSEVENgain();
     APRseven += aprsevengain * 0.05;
     updateUI();
