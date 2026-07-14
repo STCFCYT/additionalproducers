f6327e6844e2ea766f9d4021ea44880e152bbf55
STCFCYT <pspindler@gmail.com>
2026-07-13 21:54:17 -0700

    Simplify TICKSPEED function calculation

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -494,7 +494,7 @@ function APRTWO() {
 }
 function TICKSPEED() {
     if (AP >= tickspeedcost) {
-        tickspeed *= (tickspeedboostmult * 1.15);
+        tickspeed *= 1.15;
         AP -= tickspeedcost
         tickspeedcost *= 10;
     }
